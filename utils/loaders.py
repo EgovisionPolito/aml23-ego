73fa3e9dba03cf8dd97cc4c970ec368a9f8ce2b8
Emilio Joseph Grieco s317869 <s317869@studenti.polito.it>
2024-01-22 14:36:25 +0100

    fix: case where record_num_frames < 2*clip_radius (Dense Sampling)

--- utils/loaders.py
+++ utils/loaders.py
@@ -96,7 +96,7 @@ class EpicKitchensDataset(data.Dataset, ABC):
         sampled_frames_inidices_list = []
 
         ##* DENSE Sampling
-        if(record_num_frames > desired_num_frames):
+        if record_num_frames > desired_num_frames:
             for clip_number in range(self.num_clips):
                     clip_central_point = np.random.randint(clip_radius, record_num_frames-clip_radius+2) # se record_num_frames=80 e cp=64 => sampled_frames_inidices_list=[48,..,64,..78], per questo il +2
                     clip_sampled_frames_inidices = list(range(clip_central_point-clip_radius, clip_central_point+clip_radius, frames_interval))
@@ -106,6 +106,7 @@ class EpicKitchensDataset(data.Dataset, ABC):
             offset = desired_num_frames - record_num_frames
             clip_sampled_frames_inidices = list(index for index in range(0, record_num_frames))
             clip_sampled_frames_inidices.extend(index for index in range(0, offset))
+            sampled_frames_inidices_list.extend(clip_sampled_frames_inidices)
 
             #     logger.info(f"Record_num_frames {record_num_frames}, clip_radius {clip_radius}, (record_num_frames-clip_radius+2)<=clip_radius = {(record_num_frames-clip_radius+2)}")
             #*Se volessimo una lista di array numpy dove ogni array ha i frame di una clip
@@ -115,7 +116,7 @@ class EpicKitchensDataset(data.Dataset, ABC):
         if(len(sampled_frames_inidices_list) == desired_num_frames):
             return sampled_frames_inidices_list
         else:
-            raise SystemError(f"For the record {record.untrimmed_video_name}, the number of extracted frames is less than the desired {desired_num_frames} frames!")
+            raise SystemError(f"For the record {record.untrimmed_video_name}, the number of extracted frames is {len(sampled_frames_inidices_list)}less than the desired {desired_num_frames} frames!")
 
         ##* UNIFORM Sampling
 
