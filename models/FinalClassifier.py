4345e7c6e7d8ba425afc75f2a8da5bd45b25f030
Emilio Joseph Grieco s317869 <s317869@studenti.polito.it>
2024-02-03 18:44:15 +0100

    fix: LSTM input params

--- models/FinalClassifier.py
+++ models/FinalClassifier.py
@@ -36,7 +36,7 @@ class LSTM(nn.Module):
         self.input_size = 1024
         self.hidden_size = 128
         self.num_layers = 1
-        self.lstm = nn.LSTM(self.input_size, self.hidden_size, self.num_layers, num_classes, batch_first=True)
+        self.lstm = nn.LSTM(self.input_size, self.hidden_size, num_classes, self.num_layers, batch_first=True)
 
     def forward(self, x):
         logger.info(f"x parameter: {x}")
