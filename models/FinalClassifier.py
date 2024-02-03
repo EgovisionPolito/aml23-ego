d4446112e05c6d711634ff1caaba45636149bc66
Emilio Joseph Grieco s317869 <s317869@studenti.polito.it>
2024-02-03 23:39:07 +0100

    fix: change x dimension in LSTM

--- models/FinalClassifier.py
+++ models/FinalClassifier.py
@@ -29,10 +29,11 @@ class LSTM(nn.Module):
 
     def forward(self, x):
         #* we want x shape equal to (batch_size, sequence_length, input_size)
-        x.reshape((self.batch_size, self.sequence_length, 32))
-        logger.info(f"x parameter: {x}, x_shape: {x.shape}")
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        reshaped_x = x.reshape((self.batch_size, 1, 32))
+        # x.reshape((self.batch_size, self.sequence_length, self.input_size))
+        logger.info(f"x: {reshaped_x}, x_shape: {reshaped_x.shape}")
+        h0 = torch.zeros(self.num_layers, reshaped_x.size(0), self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_layers, reshaped_x.size(0), self.hidden_size).to(device)
         # logger.info(f"INITIAL - x: {x}, x_length: {len(x)}, x_shape: {x.shape}, h0: {h0.shape}, c0: {c0.shape}")
 
         # x = x.unsqueeze(0)
@@ -41,7 +42,7 @@ class LSTM(nn.Module):
         # x = x.transpose(1, 2)
         # logger.info(f"TRANSPOSE - x: {x}, x_length: {len(x)}, x_shape: {x.shape}, h0: {h0.shape}, c0: {c0.shape}")
 
-        out, _ = self.lstm(x, (h0, c0))
+        out, _ = self.lstm(reshaped_x, (h0, c0))
         # out = out[:, -1, :]
         # reshaped_x = x.reshape(self.batch_size, self.sequence_length, -1)
         # lstm_out, _ = self.lstm(x)
