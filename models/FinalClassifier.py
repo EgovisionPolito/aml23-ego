39418403dc56e6d0aa9c5e0f7dba439e495e600e
Emilio Joseph Grieco s317869 <s317869@studenti.polito.it>
2024-02-04 23:44:26 +0100

    test

--- models/FinalClassifier.py
+++ models/FinalClassifier.py
@@ -44,8 +44,12 @@ class LSTM(nn.Module):
         # out: contains the output features (batch_size, sequence_length, hidden_size)=(32, 1, 32)
         # hn: final hidden state for each element in sequence, stessa size di h0
         # cn: final cell state for each element in sequence, stessa di c0
-        feat = hn[-1]
-        logits = self.fc(out) #-> forse per le logits
+        last_output = out[:, -1, :]
+        logits = self.fc(last_output)
+        last_hn = out[:, -1, :]
+        feat = self.fc(last_hn)
+        #feat = hn[-1] #(32, 32)
+        #logits = self.fc(out) #(32, 8)
         logger.info(f"hn: {hn}, hn[-1]=feat: {feat}, shape: {feat.shape}, logits: {logits}, shape: {logits.shape}")  #logits: tipo le label, cioè le previsioni tipo
 
-        return logits, {"features": feat} #(1, 1024), (1, 8)
+        return logits, {"features": feat} #(1, 1024), (batch_size, 1, 8)
